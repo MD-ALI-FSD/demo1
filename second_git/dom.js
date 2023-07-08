4ab5e8d04b509b69bb88bd1625df8f4b26f42ec4
MD-ALI-FSD <mohammed.ali.fsd@gmail.com>
2023-07-08 10:06:11 +0530

    second commit

---
+++ second_git/dom.js
@@ -0,0 +1,6 @@
+const header = document.getElementById("header-title");
+header.style.borderBottom = "solid 3px #000";
+
+const sechead = document.querySelector(".title");
+sechead.style.color = "red";
+// sechead.bold();
