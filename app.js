f1e344f59a6a5a8c79ef195056e25189a49b4820
MD-ALI-FSD <mohammed.ali.fsd@gmail.com>
2023-07-08 11:23:32 +0530

    3rd

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log("second git");
+const items = document.querySelector(".list-group");
+items.style.fontWeight = "bold";
+items.children[2].style.color = "blue";
--- second_git/dom.js
+++
@@ -1,6 +0,0 @@
-const header = document.getElementById("header-title");
-header.style.borderBottom = "solid 3px #000";
-
-const sechead = document.querySelector(".title");
-sechead.style.color = "red";
-// sechead.bold();
